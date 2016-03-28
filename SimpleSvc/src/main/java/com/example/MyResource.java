c6d8389a3dd0d80387898c8ba1ff0c613c19586b
William Li <sjtuliwei@gmail.com>
2016-03-28 16:16:48 +0800

    Added SLF4j and Logback

--- SimpleSvc/src/main/java/com/example/MyResource.java
+++ SimpleSvc/src/main/java/com/example/MyResource.java
@@ -5,11 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
 public class MyResource {
+	
+	final Logger logger = LoggerFactory.getLogger(MyResource.class);
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -20,6 +25,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
+    	logger.info("MyResource is invoking ...");
         return "Got it!";
     }
 }
